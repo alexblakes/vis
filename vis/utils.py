07550382719f65a461bf6a9074ab789fd9c6e679
Alexander Blakes <64164119+alexblakes@users.noreply.github.com>
2025-11-23 15:47:18 +0000

    Update utils.py

--- vis/utils.py
+++ vis/utils.py
@@ -21,7 +21,7 @@ def rotate_tick_labels(axis, ax=None, **kwargs):
 
 def configure_annotator(annot, *args, **kwargs):
     kwargs.setdefault("show_test_name", False)
-    kwargs.setdefault("text_format", "simple") # Use "full" for exact P values smaller than the threshold
+    kwargs.setdefault("text_format", "simple") # Use "full" for exact P values below the threshold
     kwargs.setdefault("pvalue_thresholds", [[1e-16, "1e-16"]])
     kwargs.setdefault("pvalue_format_string", "{:.3g}")
     kwargs.setdefault("p_capitalized", True)
